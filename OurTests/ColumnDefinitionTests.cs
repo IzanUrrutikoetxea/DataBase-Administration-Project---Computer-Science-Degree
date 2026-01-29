7e04687cee14725baeffc875cd71bfe78fa71a84
Izan <izan@lasadynamics.com>
2026-01-29 08:39:19 +0100

    constructor test implemented

--- OurTests/ColumnDefinitionTests.cs
+++ OurTests/ColumnDefinitionTests.cs
@@ -1,14 +1,22 @@
+using DbManager;
 namespace OurTests
 {
-    public class ColumnDefinitionsTests
+  public class ColumnDefinitionsTests
+  {
+    [Theory]
+    [InlineData(ColumnDefinition.DataType.String, "ColumnA")] 
+    [InlineData(ColumnDefinition.DataType.Int, "ColumnB")]
+    [InlineData(ColumnDefinition.DataType.Double, "")]
+    [InlineData(ColumnDefinition.DataType.String, "12345")]
+    public void ColumnDefinition_Constructor_SetsPropertiesCorrectly(ColumnDefinition.DataType type, string name)
     {
-        //TODO DEADLINE 1A : Create your own tests for Table
-        /*
-        [Fact]
-        public void Test1()
-        {
-
-        }
-        */
+      // Arrange & Act
+      var columnDefinition = new ColumnDefinition(type, name);
+      // Assert
+      Assert.Equal(type, columnDefinition.Type);
+      Assert.Equal(name, columnDefinition.Name);
     }
+
+
+  }
 }
