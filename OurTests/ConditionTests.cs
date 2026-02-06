1cd6e4106028428f45e44104b9e3214670ed608d
Izan <izan@lasadynamics.com>
2026-02-06 21:39:49 +0100

    #4 Condition Istrue function and its tests implementation

--- OurTests/ConditionTests.cs
+++ OurTests/ConditionTests.cs
@@ -36,5 +36,45 @@ namespace OurTests
       Assert.Null(condition.LiteralValue);
     }
     #endregion
+    #region IsTrue Tests
+    [Theory]
+    [InlineData (true, "hello", "=", "hello", DbManager.ColumnDefinition.DataType.String)]
+    [InlineData (false, "hello", "=", "hi", DbManager.ColumnDefinition.DataType.String)]
+    [InlineData (true, "hello", "!=", "hi", DbManager.ColumnDefinition.DataType.String)]
+    [InlineData (false, "hello", "!=", "hello", DbManager.ColumnDefinition.DataType.String)]
+    [InlineData (false, "a", ">", "b", DbManager.ColumnDefinition.DataType.String)]
+    [InlineData (true, "b", ">", "a", DbManager.ColumnDefinition.DataType.String)]
+    [InlineData (true, "a", "<", "b", DbManager.ColumnDefinition.DataType.String)]
+    [InlineData (false, "b", "<", "a", DbManager.ColumnDefinition.DataType.String)]
+    [InlineData (true, "2", "=", "2", DbManager.ColumnDefinition.DataType.Int)]
+    [InlineData (false, "2", "=", "3", DbManager.ColumnDefinition.DataType.Int)]
+    [InlineData (true, "2", "!=", "3", DbManager.ColumnDefinition.DataType.Int)]
+    [InlineData (false, "2", "!=", "2", DbManager.ColumnDefinition.DataType.Int)]
+    [InlineData (false, "2", ">", "3", DbManager.ColumnDefinition.DataType.Int)]
+    [InlineData (true, "2", ">", "1", DbManager.ColumnDefinition.DataType.Int)]
+    [InlineData (true, "2", "<", "3", DbManager.ColumnDefinition.DataType.Int)]
+    [InlineData (false, "2", "<", "1", DbManager.ColumnDefinition.DataType.Int)]
+    [InlineData (true, "2.3", "=", "2.3", DbManager.ColumnDefinition.DataType.Double)]
+    [InlineData (false, "2.3", "=", "3.3", DbManager.ColumnDefinition.DataType.Double)]
+    [InlineData (true, "2.3", "!=", "3.3", DbManager.ColumnDefinition.DataType.Double)]
+    [InlineData (false, "2.3", "!=", "2.3", DbManager.ColumnDefinition.DataType.Double)]
+    [InlineData (false, "2.3", ">", "3.3", DbManager.ColumnDefinition.DataType.Double)]
+    [InlineData (true, "2.3", ">", "1.3", DbManager.ColumnDefinition.DataType.Double)]
+    [InlineData (true, "2.3", "<", "3.3", DbManager.ColumnDefinition.DataType.Double)]
+    [InlineData (false, "2.3", "<", "1.3", DbManager.ColumnDefinition.DataType.Double)]
+    public void Condition_Constructor_ShouldReturnTheCorrectResultOfOperation(bool expectedResult, string value, string op, string valueCompar, DbManager.ColumnDefinition.DataType type)
+    {
+      //Arrange
+      string columnName = "Name";
+      var condition = new DbManager.Condition(columnName, op, valueCompar);
+
+      //Act
+      var result = condition.IsTrue(value, type);
+
+      //Assert
+      Assert.Equal(expectedResult, result);
+    }
+    #endregion
+
   }
 }
