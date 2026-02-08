cf9128ce4907a9abdbe4811da9674152397a5f44
Izan <izan@lasadynamics.com>
2026-02-09 00:28:47 +0100

    #17 IsPasswordCorrect function and it tests implemented

--- OurTests/ManagerTests.cs
+++ OurTests/ManagerTests.cs
@@ -32,5 +32,47 @@ namespace OurTests
       Assert.False(result);
     }
     #endregion
+
+    #region IsPasswordCorrect Tests
+    [Fact]
+    public void ManagerTests_IsPasswordCorrect_ShouldReturnTrue_WhenPasswordIsCorrect()
+    {
+      //Arrange
+      var username = "User";
+      var password = "user";
+      var user = new User(username, password);
+
+      var profile = new Profile();
+      profile.Users.Add(user);
+
+      var manager = new Manager(username);
+      manager.Profiles.Add(profile);
+
+      //Act
+      var result = manager.IsPasswordCorrect(username, password);
+
+      //Arrange
+      Assert.True(result);
+    }
+    [Fact]
+    public void ManagerTests_IsPasswordCorrect_ShouldReturnFalse_WhenPasswordIsIncorrect()
+    {
+      //Arrange
+      var username = "User";
+      var password = "user";
+      var user = new User(username, password);
+
+      var profile = new Profile();
+      profile.Users.Add(user);
+
+      var manager = new Manager(username);
+
+      //Act
+      var result = manager.IsPasswordCorrect(username, password + "a");
+
+      //Arrange
+      Assert.False(result);
+    }
+    #endregion
   }
 }
