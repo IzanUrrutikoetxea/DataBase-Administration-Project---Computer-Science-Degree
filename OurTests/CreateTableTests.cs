74ff8481088c6114197297f2ca8e0df668a655df
Izan <izan@lasadynamics.com>
2026-02-11 13:13:41 +0100

    #12 CreateTable execute function and its test implemented

--- OurTests/CreateTableTests.cs
+++ OurTests/CreateTableTests.cs
@@ -1,3 +1,5 @@
+using DbManager;
+
 namespace OurTests
 {
   public class CreateTableTests
@@ -8,12 +10,42 @@ namespace OurTests
     {
       //Arrange
       var table = "TestTable";
+      var columns = new List<ColumnDefinition>()
+      {
+        (new ColumnDefinition(ColumnDefinition.DataType.String, "Name")),
+        (new ColumnDefinition(ColumnDefinition.DataType.Int, "Age"))
+      };
 
       //Act
-      var dropTable = new DbManager.DropTable(table);
+      var createTable = new DbManager.CreateTable(table, columns);
 
       //Assert
-      Assert.Equal(table, dropTable.Table);
+      Assert.Equal(table, createTable.Table);
+      Assert.Equal(columns.Count, createTable.ColumnsParameters.Count);
+    }
+    #endregion
+
+    #region Execute tests
+    [Fact]
+    public void CreateTable_Execute_ShouldWorkCorrectly()
+    {
+      //Arrange
+      var database = Database.CreateTestDatabase();
+      var tableName = "TestingTable";
+      var columns = new List<ColumnDefinition>()
+      {
+        (new ColumnDefinition(ColumnDefinition.DataType.String, "Name")),
+        (new ColumnDefinition(ColumnDefinition.DataType.Int, "Age"))
+      };
+      var table = new Table(tableName, columns);
+      var createTable = new DbManager.CreateTable(tableName, columns);
+
+      //Act
+      var result = createTable.Execute(database);
+
+      //Assert
+      Assert.Equal(table.ToString(), database.TableByName(tableName).ToString());
+      Assert.Equal(Constants.CreateTableSuccess, result);
     }
     #endregion
   }
